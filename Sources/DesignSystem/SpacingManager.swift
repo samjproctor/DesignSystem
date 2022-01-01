9e97da6ba23c087d379d40e7616a686e8e70dc11
Sam Proctor <info@zerodevteam.co.uk>
2022-01-01 17:24:35 +0000

    Spacing is CGFloat

--- Sources/DesignSystem/SpacingManager.swift
+++ Sources/DesignSystem/SpacingManager.swift
@@ -8,13 +8,13 @@
 import Foundation
 import UIKit
 
-public enum Spacing: Float {
+public enum Spacing: CGFloat {
     case small = 4
     case medium = 8
     case large = 16
     case superLarge = 24
 
-    public var name: Float {
+    public var name: CGFloat {
         return self.rawValue
     }
 }
